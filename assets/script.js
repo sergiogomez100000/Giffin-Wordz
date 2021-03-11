0a3f67782d1542ba4b169c0ae853183d53ca33cc
Cole Winslow <colethomaswinslow7@gmail.com>
2021-03-11 13:32:57 -0700

    added a gif to the page

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,22 @@
 // create API key for gif
 const apiKeyGiphy = "F9EV2crSkhvqoTLa3t0KEM15VzdKISVG";
+const gifContainer = document.querySelector("#gif-container");
 
 function getGif(searchQuery) {
-  const giphyUrl = `https://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=${apiKeyGiphy}`;
-  console.log(giphyUrl)
+  const giphyUrl = `https://api.giphy.com/v1/gifs/random?tag=${searchQuery}&api_key=${apiKeyGiphy}`;
+  console.log(giphyUrl);
   fetch(giphyUrl)
+    .then((data) => data.json())
     .then((data) => {
       console.log(data);
+      var imgEl = document.createElement("img")
+      imgEl.setAttribute("src", data.data.image_url)
+     gifContainer.append(imgEl)
+      console.log(typeof imgEl)
     });
 }
 getGif("cats");
+
 // create API key for Dictionary
 // var input on form
 // create function to access API info gif (fetch and var for url)
