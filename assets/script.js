fae0d2add2c601a2f54303ef14494285451c54a8
Theresa Pidutti <treepidutti@gmail.com>
2021-03-11 10:20:21 -0700

    update psuedo

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// create API key for gif
+// create API key for Dictionary
+// var input on form 
+// create function to access API info gif (fetch and var for url)
+// create function to access API info dictionary (fetch and var for url)
+// local storage for gif favorite button
+// append and/or local storage for word that was searched
+// event listener for submit button
+// event listener for favorite button
