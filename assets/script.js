da8a1bd73f5677a3e9c07d81cdf1b6d369037139
Theresa Pidutti <treepidutti@gmail.com>
2021-03-11 14:07:59 -0700

    update gify

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 // create API key for gif
 const apiKeyGiphy = "F9EV2crSkhvqoTLa3t0KEM15VzdKISVG";
 const gifContainer = document.querySelector("#gif-container");
+const searchBar = document.querySelector("#searchBar");
+const submitBtn = document.querySelector("#submitBtn");
 
 function getGif(searchQuery) {
   const giphyUrl = `https://api.giphy.com/v1/gifs/random?tag=${searchQuery}&api_key=${apiKeyGiphy}`;
@@ -9,13 +11,22 @@ function getGif(searchQuery) {
     .then((data) => data.json())
     .then((data) => {
       console.log(data);
-      var imgEl = document.createElement("img")
+      const imgEl = document.createElement("img")
       imgEl.setAttribute("src", data.data.image_url)
+      gifContainer.innerHTML = ""
      gifContainer.append(imgEl)
       console.log(typeof imgEl)
     });
 }
-getGif("cats");
+
+function handleSubmit(){
+  const input = searchBar.value;
+  getGif(input)
+
+  console.log(input)
+}
+submitBtn.addEventListener("click", handleSubmit);
+// getGif("cats");
 
 // create API key for Dictionary
 // var input on form
