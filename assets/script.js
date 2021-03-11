491733693e8f1847aff1b808f3ddb27ddf9b32f1
Cole Winslow <colethomaswinslow7@gmail.com>
2021-03-11 12:23:46 -0700

    fetch for giphy api

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,17 @@
 // create API key for gif
+const apiKeyGiphy = "F9EV2crSkhvqoTLa3t0KEM15VzdKISVG";
+
+function getGif(searchQuery) {
+  const giphyUrl = `https://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=${apiKeyGiphy}`;
+  console.log(giphyUrl)
+  fetch(giphyUrl)
+    .then((data) => {
+      console.log(data);
+    });
+}
+getGif("cats");
 // create API key for Dictionary
-// var input on form 
+// var input on form
 // create function to access API info gif (fetch and var for url)
 // create function to access API info dictionary (fetch and var for url)
 // local storage for gif favorite button
