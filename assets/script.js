20420041056555136576c2e492939090edd834c2
Nate Shipp <nateskate256@gmail.com>
2021-03-13 10:33:38 -0700

    Styles and adjusted Elements

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const apiKeyGiphy = "F9EV2crSkhvqoTLa3t0KEM15VzdKISVG";
 const gifContainer = document.querySelector("#gif-container");
 //create variable for def container
 const defContainer = document.querySelector("#def-container");
-//create var for searchbar 
+//create var for searchbar
 const searchBar = document.querySelector("#searchBar");
 //create variable for submitBtn
 const submitBtn = document.querySelector("#submitBtn");
@@ -14,12 +14,12 @@ const favBtn = document.querySelector("#favBtn");
 let currentGifUrl = "";
 
 // create var favArr assigning the parsed stringified array in local storage under favGifsSave, reading this from favGifsSave and if nothing there reads empty array
-function getFavArr (){
-return JSON.parse(localStorage.getItem("favGifsSave")) || [];
+function getFavArr() {
+  return JSON.parse(localStorage.getItem("favGifsSave")) || [];
 }
-let favArr = getFavArr()
+let favArr = getFavArr();
 
-const apiKeyWebster = "b8d63f50-7aed-4699-a007-51ec78791ac1"
+const apiKeyWebster = "b8d63f50-7aed-4699-a007-51ec78791ac1";
 
 function getGif(searchQuery) {
   const giphyUrl = `https://api.giphy.com/v1/gifs/random?tag=${searchQuery}&api_key=${apiKeyGiphy}`;
@@ -28,65 +28,61 @@ function getGif(searchQuery) {
     //convert data into .json making it readable
     .then((data) => data.json())
     .then((data) => {
-      currentGifUrl = data.data.image_url
+      currentGifUrl = data.data.image_url;
       //console.log(data);
-      const imgEl = document.createElement("img")
-      imgEl.setAttribute("src", currentGifUrl)
-      gifContainer.innerHTML = ""
-     gifContainer.append(imgEl)
+      const imgEl = document.createElement("img");
+      imgEl.setAttribute("src", currentGifUrl);
+      gifContainer.innerHTML = "";
+      gifContainer.append(imgEl);
       //console.log(typeof imgEl)
     });
 }
 
-function getDefinition(searchQuery){
-  const websterUrl= `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchQuery}?key=${apiKeyWebster}`
+function getDefinition(searchQuery) {
+  const websterUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchQuery}?key=${apiKeyWebster}`;
   fetch(websterUrl)
-  .then((data) => data.json())
-  .then((data) => {
-    console.log(data);
-    // targets short def in search query
-    console.log(data[0])
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(data);
+      // targets short def in search query
+      console.log(data[0]);
 
-    //empties defContainer
-    defContainer.innerHTML = ""
+      //empties defContainer
+      defContainer.innerHTML = "";
 
-    const h1El = document.createElement("h1")
-    h1El.textContent = searchBar.value
-    defContainer.append(h1El)
+      const h1El = document.createElement("h1");
+      h1El.textContent = searchBar.value;
+      defContainer.append(h1El);
 
-    //creates p tag el  //updates defEL text content to array // appends defEL (adds) to defContainer
-    const defEl = document.createElement("p")
-    defEl.textContent ="Definition: " + (data[0].shortdef[0])
-    defContainer.append(defEl);
+      //creates p tag el  //updates defEL text content to array // appends defEL (adds) to defContainer
+      const defCaseEl = document.createElement("p");
+      defCaseEl.textContent = "Case: " + data[0].fl;
+      //console.log(data[0].fl)
+      defContainer.append(defCaseEl);
 
-     
-    const defCaseEl= document.createElement("p")
-    defCaseEl.textContent = "Case: " + (data[0].fl)
-    //console.log(data[0].fl)
-    defContainer.append(defCaseEl)
+      const defEl = document.createElement("p");
+      defEl.textContent = "Definition: " + data[0].shortdef[0];
+      defContainer.append(defEl);
 
-    const defAudio = data[0].hwi.prs[0]
-    const defAudioUrl =`https://media.merriam-webster.com/audio/prons/en/us/mp3/${defAudio.sound.audio[0]}/${defAudio.sound.audio}.mp3`
-    const defAudioEl = document.createElement("audio")
-    defAudioEl.setAttribute("src", defAudioUrl)
-    defAudioEl.setAttribute("controls","")
-    defContainer.append(defAudioEl)
-    
-    // targets pronunciation from data
-    console.log(data[0].hwi.prs[0])
-    const defProEl = document.createElement("p")
-    defProEl.textContent = ["Pronunciation: " + (defAudio.mw)]
-    defContainer.append(defProEl)
-    
+      // targets pronunciation from data
+      const defAudio = data[0].hwi.prs[0];
+      console.log(data[0].hwi.prs[0]);
+      const defProEl = document.createElement("p");
+      defProEl.textContent = ["Pronunciation: " + defAudio.mw];
+      defContainer.append(defProEl);
 
-    
-
-  })
+      
+      const defAudioUrl = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${defAudio.sound.audio[0]}/${defAudio.sound.audio}.mp3`;
+      const defAudioEl = document.createElement("audio");
+      defAudioEl.setAttribute("src", defAudioUrl);
+      defAudioEl.setAttribute("controls", "");
+      defContainer.append(defAudioEl);
+    });
 }
-function handleSubmit(){
+function handleSubmit() {
   const input = searchBar.value;
-  getGif(input)
-  getDefinition(input)
+  getGif(input);
+  getDefinition(input);
   //console.log(input)
 }
 submitBtn.addEventListener("click", handleSubmit);
@@ -94,12 +90,10 @@ submitBtn.addEventListener("click", handleSubmit);
 favBtn.addEventListener("click", handleSave);
 
 // when fav button hit, push fav gif array into local storage, array push method, then restore with stringify
-function handleSave(){
-  favArr = getFavArr()
+function handleSave() {
+  favArr = getFavArr();
   // if current gif url is empty or has something in it get out
-if(currentGifUrl === "" || favArr.includes(currentGifUrl)) return;
-favArr.push(currentGifUrl)
-localStorage.setItem("favGifsSave", JSON.stringify(favArr))
+  if (currentGifUrl === "" || favArr.includes(currentGifUrl)) return;
+  favArr.push(currentGifUrl);
+  localStorage.setItem("favGifsSave", JSON.stringify(favArr));
 }
-
-
